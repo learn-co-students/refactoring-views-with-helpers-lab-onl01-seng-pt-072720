69698b81d57fa5ef2a9a76b6643396bfc3300e3b
fentyhall <fentymhall@gmail.com>
2020-12-11 14:37:46 -0800

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,6 +1,6 @@
 module ArtistsHelper
     def display_artist(song)
-        if song.artist.present?
+        if song.artist.present? && song.artist.name != ""
             link_to song.artist.name, artist_path(song.artist)
         else 
             link_to "Add Artist", edit_song_path(song)
